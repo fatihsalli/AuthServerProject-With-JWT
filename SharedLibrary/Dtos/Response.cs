c78cd84004bc539fb9f7fdec762508f42de722c6
Fatih Şallı <mimsallifatih@gmail.com>
2022-10-27 16:08:46 +0300

    Response

--- SharedLibrary/Dtos/Response.cs
+++ SharedLibrary/Dtos/Response.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace SharedLibrary.Dtos
 {
@@ -10,23 +6,35 @@ namespace SharedLibrary.Dtos
     {
         public T Data { get; private set; }
         public int StatusCode { get; private set; }
-        public ErrorDto Error { get; set; }
+        //Client tarafında açmayacağız. Kendi içimizde Apiler arasında olayın başarılı olup olmadığını kısa yoldan görmek için tanımladık. Json dataya serialize edildiğinde ignore edilecek. Client tarafına göstermemek kendi içimizde kullanmak için.
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+        public ErrorDto Error { get; private set; }
 
         //Static factory method tanımladık factory desing patternden gelir.(Nesne üretme yolu)
         public static Response<T> Success(T data,int statusCode)
         {
-            return new Response<T> { Data = data, StatusCode = statusCode };
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
         }
 
         public static Response<T> Success(int statusCode)
         {
-            return new Response<T> { Data = default, StatusCode = statusCode };
+            return new Response<T> { Data=default, StatusCode = statusCode, IsSuccessful = true };
         }
 
         public static Response<T> Fail(ErrorDto errorDto, int statusCode)
         {
-            return new Response<T> { Error = errorDto, StatusCode = statusCode };
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
         }
+
+        public static Response<T> Fail(string errorMessage, int statusCode,bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage,isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+
+
+
 
 
     }
