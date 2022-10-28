98aa73b95fcd8f242a6116ff6518b36fd730b192
Fatih Şallı <mimsallifatih@gmail.com>
2022-10-28 14:43:59 +0300

    AppDbContext

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AuthServer.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    //DbContext yerine IdentityDbContext'ten miras aldık. Kendi oluşturduğumuz "UserApp" i ve IdentityRole (Identity kütüphanesinden gelen) ve Id'yi nin tipini belirttik.
+    public class AppDbContext : IdentityDbContext<UserApp,IdentityRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Product> Products { get; set; }
+        //Access tokenları değil Refresh tokenları serverda tutacağız.
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+
+
+    }
+}
