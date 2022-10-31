3d52ae25273b0b966c660829e5138833f090dda5
Fatih Şallı <mimsallifatih@gmail.com>
2022-10-31 11:45:03 +0300

    AutoMapper

--- AuthServer.Core/Services/IAuthenticationService.cs
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -13,7 +13,7 @@ namespace AuthServer.Core.Services
         //Asenkron metot threadleri daha efektif kullanmamızı sağlayan metottur.
         Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
         Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
-        //Kullanıcı log-out yaptığında Refresh tokenı sonlandırma durumu için aşağıdaki metot tanımlandı. Ya da Refreshtoken çalındığı durumda bu metot ile server tarafında refresh tokenı nulla set edebiliriz.
+        //Kullanıcı log-out yaptığında Refresh tokenı sonlandırma durumu için aşağıdaki metot tanımlandı. Ya da Refreshtoken çalındığı durumda bu metot ile server tarafında refresh tokenı null'a set edebiliriz.
         Task<Response<NoDataDto>> RevokeRefreshTokenAsync(string refreshToken);
         //Client ile birlikte üyelik durumu olmadan bir token alabiliriz. Aşaığıdaki metot bu durum için tanımlanmıştır. İçerisinde bir RefreshToken yok çünkü ClientId ve ClientSecret ile ben istediğim zaman Access Token alabilirim. Biz Client tarafında Client Id ve Secret bilgilerini dizin şeklinde AuthServer.Api-app.settings içinde tutacağız ancak 5 ten fazla olması durumunda serverda tutmak gerekir.
         Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
---
+++ AuthServer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,21 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+
+        }
+    }
+}
