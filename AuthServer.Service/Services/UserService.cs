3ed523682757d2c10415a22d8c8958e65bfe7126
Fatih Şallı <mimsallifatih@gmail.com>
2022-10-31 19:14:17 +0300

    UserService

---
+++ AuthServer.Service/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using AuthServer.Service.Mapping;
+using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            var user = new UserApp
+            {
+                Email = createUserDto.Email,
+                UserName = createUserDto.UserName
+            };
+            //Passwordu hashleyip kendisi dolduruyor.
+            var result=await _userManager.CreateAsync(user, createUserDto.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x=> x.Description).ToList();
+                return Response<UserAppDto>.Fail(new ErrorDto(errors, true), 400);
+            }
+
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user),200);
+        }
+
+        public async Task<Response<UserAppDto>> GetUserByNameAsync(string userName)
+        {
+            var user=await _userManager.FindByNameAsync(userName);
+
+            if (user==null)
+            {
+                return Response<UserAppDto>.Fail("UserName not found", 404,true);
+            }
+
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), 200);
+        }
+    }
+}
