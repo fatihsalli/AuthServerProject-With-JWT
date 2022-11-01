105a1f93fa5a897a3cffcc0cbe73a4cb3d5397d0
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-01 12:30:22 +0300

    .

---
+++ SharedLibrary/Extensions/CustomTokenAuth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Extensions
+{
+    //Json web token doğrulamasına diğer apilerde ulaşabilmek adına ortak bir extension class oluşturduk. Buradan Apilere hizmet edecek.
+    public static class CustomTokenAuth
+    {
+
+
+
+    }
+}
