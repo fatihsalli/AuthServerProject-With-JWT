121969a58646703c2842d4dbf92721b85e231b62
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-02 19:41:49 +0300

    Roles

--- MiniApp1.API/Controllers/StockController.cs
+++ MiniApp1.API/Controllers/StockController.cs
@@ -6,7 +6,7 @@ using System.Security.Claims;
 
 namespace MiniApp1.API.Controllers
 {
-    [Authorize]
+    [Authorize(Roles ="admin,manager")]
     [Route("api/[controller]")]
     [ApiController]
     public class StockController : ControllerBase
